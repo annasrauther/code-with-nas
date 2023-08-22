3dafb12140307a50c2f8c5489dceb7d5c8585b6b
Annas Rauther <annasrauther95@gmail.com>
2023-08-22 10:53:41 +0530

    perf: optimized critical rendering of google fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,14 +9,50 @@ class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Add a link to import fonts from Google Fonts */}
-          <link
+          {/**
+             * Preload and load fonts using various methods.
+             * 
+             * For more information, see:
+             * {@link https://csswizardry.com/2020/05/the-fastest-google-fonts/ The Fastest Google Fonts}
+          */}
+
+          {/**
+             * Preemptively warm up the fonts’ origin.
+          */}
+          <link rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossorigin />
+
+          {/**
+             * Initiate a high-priority, asynchronous fetch for the CSS file. Works in most modern browsers.
+          */}
+          <link rel="preload"
+            as="style"
+            href="https://fonts.googleapis.com/css2?family=Raleway&family=Bricolage+Grotesque&display=swap" />
+
+          {/**
+             * Initiate a low-priority, asynchronous fetch that gets applied to the page
+             *    only after it’s arrived. Works in all browsers with JavaScript enabled.
+          */}
+          <link rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Raleway&family=Bricolage+Grotesque&display=swap"
-            rel="stylesheet"
-          />
+            media="print" onload="this.media='all'" />
+
+          {/**
+             * In the unlikely event that a visitor has intentionally disabled
+             *    JavaScript, fall back to the original method. The good news is that,
+             *    although this is a render-blocking request, it can still make use of the
+             *    preconnect which makes it marginally faster than the default.
+          */}
+          <noscript>
+            <link rel="stylesheet"
+              href="https://fonts.googleapis.com/css2?family=Raleway&family=Bricolage+Grotesque&display=swap" />
+          </noscript>
         </Head>
         <body>
-          {/* Render the main content of the application */}
+          {/*
+            Render the main content of the application
+          */}
           <Main />
           {/* Render the Next.js script and additional scripts */}
           <NextScript />
